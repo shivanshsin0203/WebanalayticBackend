af1225eaa28c1e5c9cdd63c4197dd551b6de0c81
shivanshsin0203 <singhshivansh12may@gmail.com>
2025-02-09 17:06:43 +0530

    Connected to redis

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
-const app = new Hono()
-app.use(cors())
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+const app = new Hono();
+app.use(cors());
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.post('/items', async (c) => {
+  try {
+    const item = await c.req.json()
+    console.log(item)
+    return c.json({ id: item }, 201)
+  } catch (error) {
+    return c.json({ error: 'Failed to create item' }, 500)
+  }
 })
 
-export default app
+export default app;
