a3ec605e28040c68de9247268ad22367f812a257
shivanshsin0203 <singhshivansh12may@gmail.com>
2025-02-09 22:33:09 +0530

    Added trac and analysis route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import redis from "./redis/db";
 const app = new Hono();
 app.use(cors());
 
@@ -16,5 +17,73 @@ app.post('/items', async (c) => {
     return c.json({ error: 'Failed to create item' }, 500)
   }
 })
+app.post("/track", async (c) => {
+  
+  const body = await c.req.json();
+  console.log(body)
+  const keyId = body.keyId 
+ 
+  const sessionId = body.sessionId
+  const device =  body.device
+  const country = body.country
+  const sessionIdPresent = body.sessionIdPresent
+
+  if (!keyId || !sessionId || !device || !country) {
+    return c.text("Missing parameters!", 400);
+  }
+
+  // Increment total views
+  await redis.incr(`views:${keyId}`);
+
+  // Track unique views
+  
+  if (!sessionIdPresent) {
+    await redis.incr(`unique_views:${keyId}`);
+  }
+
+  // Increment device count
+  await redis.incr(`device:${keyId}:${device}`);
+
+  // Increment country count
+  await redis.incr(`country:${keyId}:${country}`);
+
+  await redis.sadd(`online_users:${keyId}`, sessionId);
+  await redis.expire(`online_users:${keyId}`, 300);
+
+  return c.json({ message: "View Tracked Successfully" });
+});
+app.get("/analytics", async (c) => {
+  const keyId = c.req.query("keyId");
+
+  if (!keyId) {
+    return c.text("Missing keyId!", 400);
+  }
+
+  // Fetch views & unique views
+  const views = (await redis.get(`views:${keyId}`)) || 0;
+  const uniqueViews = (await redis.get(`unique_views:${keyId}`)) || 0;
+
+  // Fetch device data
+  const mobile = (await redis.get(`device:${keyId}:mobile`)) || 0;
+  const desktop = (await redis.get(`device:${keyId}:desktop`)) || 0;
+  const tablet = (await redis.get(`device:${keyId}:tablet`)) || 0;
+
+  // Fetch country data
+  const us = (await redis.get(`country:${keyId}:US`)) || 0;
+  const india = (await redis.get(`country:${keyId}:IN`)) || 0;
+  const france = (await redis.get(`country:${keyId}:FR`)) || 0;
+
+  // Fetch online users count
+  const onlineUsers = await redis.scard(`online_users:${keyId}`) || 0;
+
+  return c.json({
+    views,
+    uniqueViews,
+    devices: { mobile, desktop, tablet },
+    countries: { us, india, france },
+    onlineUsers,
+  });
+});
+
 
 export default app;
