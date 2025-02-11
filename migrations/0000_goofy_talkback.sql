c5cbee407793169fd53e62c453c0a14c56d14a0b
shivanshsin0203 <singhshivansh12may@gmail.com>
2025-02-11 19:11:04 +0530

    Deployemet changes

--- migrations/0000_goofy_talkback.sql
+++ migrations/0000_goofy_talkback.sql
@@ -8,15 +8,10 @@ CREATE TABLE `projects` (
 	FOREIGN KEY (`user_email`) REFERENCES `users`(`email`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
-PRAGMA foreign_keys=OFF;--> statement-breakpoint
-CREATE TABLE `__new_users` (
+CREATE TABLE `users` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`email` text NOT NULL,
 	`is_active` integer DEFAULT true
 );
 --> statement-breakpoint
-INSERT INTO `__new_users`("id", "email", "is_active") SELECT "id", "email", "is_active" FROM `users`;--> statement-breakpoint
-DROP TABLE `users`;--> statement-breakpoint
-ALTER TABLE `__new_users` RENAME TO `users`;--> statement-breakpoint
-PRAGMA foreign_keys=ON;--> statement-breakpoint
 CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
